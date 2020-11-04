35784711bc8b7c558094774ec5094d5170b2f264
ziheng <ziheng@apache.org>
2020-11-03 21:08:18 -0800

    Search for int16 accumulation. (#13)

--- src/relay/qnn/op/requantize.cc
+++ src/relay/qnn/op/requantize.cc
@@ -276,9 +276,10 @@ bool RequantizeRel(const Array<Type>& types, int num_inputs, const Attrs& attrs,
     }
   }
   const auto in_dtype = data->dtype;
-  ICHECK(in_dtype == DataType::Int(8) || in_dtype == DataType::UInt(8) ||
-         in_dtype == DataType::Int(32))
-      << "Input type should be one of [int8, uint8, int32] but was " << in_dtype;
+  CHECK(in_dtype == DataType::Int(8) || in_dtype == DataType::UInt(8) ||
+        in_dtype == DataType::Int(16) || in_dtype == DataType::UInt(16) ||
+        in_dtype == DataType::Int(32))
+      << "Input type should be one of [int8, uint8, int16, uint16, int32] but was " << in_dtype;
 
   const RequantizeAttrs* requantize_attrs = attrs.as<RequantizeAttrs>();
   int axis = requantize_attrs->axis;
@@ -297,9 +298,10 @@ bool RequantizeRel(const Array<Type>& types, int num_inputs, const Attrs& attrs,
   const Array<tvm::PrimExpr> oshape = data->shape;
   // assign output type
   auto out_dtype = requantize_attrs->out_dtype;
-  ICHECK(out_dtype == DataType::Int(8) || out_dtype == DataType::UInt(8) ||
-         out_dtype == DataType::Int(32))
-      << "Output type should be one of [int8, uint8, int32] but was " << out_dtype;
+  CHECK(out_dtype == DataType::Int(8) || out_dtype == DataType::UInt(8) ||
+        out_dtype == DataType::Int(16) || out_dtype == DataType::UInt(16) ||
+        out_dtype == DataType::Int(32))
+      << "Output type should be one of [int8, uint8, int16, uint16, int32] but was " << out_dtype;
   reporter->Assign(types[5], TensorType(oshape, out_dtype));
   return true;
 }
