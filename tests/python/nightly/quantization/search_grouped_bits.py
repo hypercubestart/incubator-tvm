61120ff39ff4989add7943d3df4e40dc0442fdce
Andrew Liu <andrewlliu@gmail.com>
2021-03-12 16:23:19 -0800

    fix hardware

--- tests/python/nightly/quantization/search_grouped_bits.py
+++ tests/python/nightly/quantization/search_grouped_bits.py
@@ -30,7 +30,9 @@ resnet18_bits = [6, 7, 16, 14, 4, 16, 7, 7, 16, 14, 4, 8, 8, 16, 13, 15, 16, 4, 
 b = [6, 7, 16, 14, 4, 16, 7, 7, 16, 14, 4, 8, 8, 16, 13, 15, 16, 4, 7, 8, 16, 12, 4, 8, 8, 16, 14, 16, 16, 4, 8, 8, 15, 14, 8, 8, 16, 14, 4, 6, 8, 15, 13, 16, 16, 4, 8, 8, 16, 15, 4, 8, 8, 16, 14, 16, 16, 4, 8, 8, 16, 15, 8, 8, 16, 14, 4, 8, 7, 13, 11, 15, 16, 4, 7, 8, 14, 12, 4, 8, 8, 11, 9, 16, 14, 4, 7, 8, 15, 14, 8, 8, 16, 11, 4, 7, 8, 14, 11, 12, 11, 4, 8, 8, 12, 9, 4, 7, 8, 12, 9, 6, 7, 4]
 
 uniform8 = [8,8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8] 
+grouped_5convs = [8,8,8,8,8,8,8,8,4,4,8,8,8,8,8,8,8,8,8,8,4,4,8,8,8,8,8,8,4,4,4,4,8,8,8,8,4,4,8,8]
 
+grouped_5convs2 = [8,8,8,8,8,8,8,8,4,4,8,8,8,8,8,8,8,8,8,8,4,4,8,8,8,8,8,8,4,4,4,4,8,8,8,8,8,8,4,4]
 #####################
 # Dataset prepartions
 #####################
@@ -75,6 +77,7 @@ def create_hardware():
     #hardware.add_op_desc("concatenate", OpDesc(in_dtypes="float32", out_dtypes="float32"))
     #hardware.add_op_desc("concatenate", OpDesc(in_dtypes="int8", out_dtypes="int8"))
     #hardware.add_op_desc("concatenate", OpDesc(in_dtypes="int32", out_dtypes="int32"))
+    hardware.add_op_desc("nn.conv2d", OpDesc(in_dtypes="int4", out_dtypes="int32"))
     hardware.add_op_desc("nn.conv2d", OpDesc(in_dtypes="int8", out_dtypes="int32"))
     #hardware.add_op_desc("nn.relu", OpDesc(in_dtypes=act_dtype, out_dtypes=act_dtype))
     #hardware.add_op_desc("clip", OpDesc(in_dtypes=act_dtype, out_dtypes=act_dtype))
@@ -121,15 +124,15 @@ def main():
         print(fp32_mod)
         qconfig = hago.qconfig(use_channel_quantize=is_per_channel,
                                round_scale_to_pot=False,
-                               log_file = 'temp.log')
+                               log_file = '/home/andy99/logs/grouped/groupedgreedy2.log')
                                # log_file='/home/andy99/logs/grouped/groupedgreedy_1000calib_1000valid.log')
                                #log_file='/home/andy99/logs/mixed_precision/strategy_{}_greedy_1-1edges.log'.format(model_name))
         
         max_trials = 20
         hardware = create_hardware()
-        bits = uniform8
+        bits = b
         validation_dataset = get_validation_dataset(val_data, batch_fn, var_name='data', offset=calib_dataset_size, num_samples=validation_dataset_size)
-        quantized_func = quantize_hago(fp32_mod, params, calib_dataset, qconfig, hardware, 'dummy', target, ctx, bits=bits, max_trials=max_trials, validation_dataset=validation_dataset)
+        quantized_func = quantize_hago(fp32_mod, params, calib_dataset, qconfig, hardware, 'groupedgreedy', target, ctx, bits=bits, max_trials=max_trials, validation_dataset=validation_dataset)
         exit()
         acc = eval_acc(quantized_func, val_data, batch_fn, args, var_name='data', target=target, ctx=ctx)
         channel_or_tensor = "per_channel" if is_per_channel else "per_tensor"
